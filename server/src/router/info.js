3f31536113447c83a26d7b99234cdb0296d5f1e2
EetuHei <t8heee01@students.oamk.fi>
2020-06-30 11:57:38 +0300

    basic router

---
+++ server/src/router/info.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const data = require("../persons.json");
+
+const infoRouter = new Router();
+
+const getAll = async (req, res, next) => {
+  try {
+    res.send(
+      `Phonebook has info for ${data.persons.length} people <br/> ${Date()}`
+    );
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+};
+
+infoRouter.get("/", getAll);
+
+module.exports = infoRouter;
