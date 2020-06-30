3cd1b0da1a02f3ec662b1d4b804700c23400cb98
EetuHei <t8heee01@students.oamk.fi>
2020-06-30 13:39:28 +0300

    updated data file, refactored the code to work with client

--- server/src/router/persons.js
+++ server/src/router/persons.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
-const data = require("../persons.json");
+const fs = require("fs");
+const data = require("../../../persons.json");
 
 const personsRouter = new Router();
 
 const getAll = async (req, res, next) => {
   try {
-    res.json(data.persons);
+    res.json(data);
   } catch (e) {
     console.error(e);
     return next(e);
@@ -15,7 +16,7 @@ const getAll = async (req, res, next) => {
 const getById = async (req, res, next) => {
   try {
     const id = Number(req.params.id);
-    const person = data.persons.find((person) => person.id === id);
+    const person = data.find((person) => person.id === id);
     res.json(person);
   } catch (e) {
     console.error(e);
@@ -26,9 +27,20 @@ const getById = async (req, res, next) => {
 const deleteById = async (req, res, next) => {
   try {
     const id = Number(req.params.id);
-    const person = data.persons.find((person) => person.id === id);
-    delete person;
-    res.data({
+    const person = data.findIndex((person) => person.id === id);
+    if (person > -1) {
+      data.splice(person, 1);
+    }
+    const jsonStr = JSON.stringify(data, null, 2);
+    fs.writeFile("persons.json", jsonStr, finished);
+    function finished(err) {
+      if (err) {
+        console.log("Error: " + err);
+      } else {
+        console.log("all gucci");
+      }
+    }
+    res.send(200, {
       data: `${person.name} was successfully deleted from the server`,
     });
   } catch (e) {
@@ -41,7 +53,7 @@ const addPerson = async (req, res, next) => {
   const randomId = Math.floor(Math.random() * Math.floor(100000));
 
   try {
-    if (data.persons.find((person) => person.name === req.body.name)) {
+    if (data.find((person) => person.name === req.body.name)) {
       res.send(400, { error: `name must be unique` });
     } else {
       const newPerson = [
@@ -51,7 +63,18 @@ const addPerson = async (req, res, next) => {
           id: randomId,
         },
       ];
+
       const newData = [...data.persons].concat(newPerson);
+      const jsonStr = JSON.stringify(newData, null, 2);
+      fs.writeFile("persons.json", jsonStr, finished);
+      function finished(err) {
+        if (err) {
+          console.log("Error: " + err);
+        } else {
+          console.log("all gucci");
+        }
+      }
+      console.log(newData);
       res.json(newData);
     }
   } catch (e) {
