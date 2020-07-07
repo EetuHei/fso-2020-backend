c995955220233f1d97a7a095900aa13a0c58cac9
EetuHei <t8heee01@students.oamk.fi>
2020-07-07 21:40:04 +0300

    moved db connection to app.js

--- index.js
+++ index.js
@@ -2,21 +2,8 @@ const app = require("./app");
 const http = require("http");
 const config = require("./src/config/config");
 const logger = require("./src/config/logger");
-const mongoose = require("mongoose")
 
 const server = http.createServer(app);
-
-mongoose
-  .connect(config.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    logger.info("connected to MongoDB");
-  })
-  .catch((error) => {
-    logger.error("error connecting to MongoDB:", error.message);
-  });
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
