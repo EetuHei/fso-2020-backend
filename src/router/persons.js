a55b55e90919f17d33e4f35f47db49c888e9f9eb
EetuHei <t8heee01@students.oamk.fi>
2020-07-07 21:42:15 +0300

    refactored persons to use express errors instead ot try, catch for testing purpose

--- src/router/persons.js
+++ src/router/persons.js
@@ -1,85 +1,54 @@
-const { Router } = require('express')
-const Persons = require('../models/persons')
+const { Router } = require("express");
+const Persons = require("../models/persons");
 
-const personsRouter = new Router()
+const personsRouter = new Router();
 
 const getAll = async (req, res, next) => {
-  try {
-    await Persons.find({}).then((result) => {
-      res.json(result)
-    })
-  } catch (e) {
-    console.error(e)
-    return next(e)
-  }
-}
+  const allPersons = await Persons.find({});
+  res.json(allPersons).end();
+};
 
 const getById = async (req, res, next) => {
-  try {
-    await Persons.findOne({ _id: req.params.id }).then((person) =>
-      res.json(person)
-    )
-  } catch (e) {
-    console.error(e)
-    return next(e)
+  const findPerson = await Persons.findOne({ _id: req.params.id });
+  if (findPerson) {
+    res.json(findPerson.toJSON());
+  } else {
+    res.status(404).end();
   }
-}
+};
 
 const deleteById = async (req, res, next) => {
-  try {
-    await Persons.deleteOne({ _id: req.params.id }).then(
-      res
-        .status(200)
-        .send('data: User was successfully deleted from the server')
-    )
-  } catch (e) {
-    console.error(e)
-    return next(e)
-  }
-}
+  await Persons.deleteOne({ _id: req.params.id });
+  res.status(204).end();
+};
 
 const addPerson = async (req, res, next) => {
-  const body = req.body
-
-  try {
-    const person = new Persons({
-      name: body.name,
-      number: body.number,
-    })
-    await person.save().then((savedPerson) => res.json(savedPerson))
-  } catch (e) {
-    console.error(e)
-    if (e.message) {
-      res.send(e)
-    } else {
-      return next(e)
-    }
-  }
-}
+  const body = req.body;
+  const person = new Persons({
+    name: body.name,
+    number: body.number,
+  });
+  const addPerson = await person.save();
+  res.json(addPerson.toJSON()).end();
+};
 
 const update = async (req, res, next) => {
   const person = {
     name: req.body.name,
     number: req.body.number,
-  }
+  };
 
-  try {
-    await Persons.findByIdAndUpdate(
-      { _id: req.params.id, new: true },
-      person
-    ).then((updatePerson) => {
-      res.json(updatePerson)
-    })
-  } catch (e) {
-    console.error(e)
-    return next(e)
-  }
-}
+  const updatePerson = await Persons.findByIdAndUpdate(
+    { _id: req.params.id, new: true },
+    person
+  );
+  res.json(updatePerson.toJSON()).end();
+};
 
-personsRouter.get('/persons', getAll)
-personsRouter.get('/persons/:id', getById)
-personsRouter.delete('/persons/delete/:id/', deleteById)
-personsRouter.post('/persons/add', addPerson)
-personsRouter.put('/persons/:id', update)
+personsRouter.get("/persons", getAll);
+personsRouter.get("/persons/:id", getById);
+personsRouter.delete("/persons/delete/:id/", deleteById);
+personsRouter.post("/persons/add", addPerson);
+personsRouter.put("/persons/:id", update);
 
-module.exports = personsRouter
+module.exports = personsRouter;
