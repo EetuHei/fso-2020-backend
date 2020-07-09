79608eed469bbf44e5ae85ab4a0814469aad02f6
EetuHei <t8heee01@students.oamk.fi>
2020-07-09 13:18:03 +0300

    simple test to see if its possible to make new user

---
+++ src/test/user_api.test.js
@@ -0,0 +1,52 @@
+const app = require("../../app");
+const supertest = require("supertest");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const helper = require("./api_test_helper");
+const mongoose = require("mongoose")
+
+const api = supertest(app);
+
+describe("when there is initially one user in db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("secret", 10);
+
+    const user = new User({
+      username: "root",
+      name: "root",
+      password: passwordHash,
+    });
+
+    await user.save();
+  });
+
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "eetuhei",
+      name: "Eetu Heikkinen",
+      password: "salainen",
+    };
+
+    await api
+      .post("/api/v1/signup")
+      .send(newUser)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+});
+
+afterAll(async (done) => {
+  // Closing the DB connection allows Jest to exit successfully.
+  mongoose.connection.close();
+  done();
+});
